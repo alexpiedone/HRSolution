c6a1da728c310e543f3194f70d125e2908fa2636
alexpiedone <alexdobre229@yahoo.ro>
2025-03-26 15:52:53 +0200

    1

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './api.service';
+import { AuthService } from './auth.service';
+import { NotificationService } from './notification.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    ApiService,
+    AuthService,
+    NotificationService
+  ]
+})
+export class CoreModule { }
