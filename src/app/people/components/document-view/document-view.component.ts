7c223769cf71768844cf5722e76cf69b1780d90b
alexpiedone <alexdobre229@yahoo.ro>
2025-03-26 17:16:54 +0200

    1

---
+++ src/app/people/components/document-view/document-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-view',
+  imports: [],
+  templateUrl: './document-view.component.html',
+  styleUrl: './document-view.component.css'
+})
+export class DocumentViewComponent {
+
+}
---
+++ src/app/people/components/leave-request/leave-request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-request',
+  imports: [],
+  templateUrl: './leave-request.component.html',
+  styleUrl: './leave-request.component.css'
+})
+export class LeaveRequestComponent {
+
+}
