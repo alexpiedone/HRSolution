832e82e9eb96fb3cd77e9578995856addb37dcf9
alexpiedone <alexdobre229@yahoo.ro>
2025-03-27 18:22:11 +0200

    1

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'YOUR_API_URL'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/login`, { username, password });
+  }
+
+  
 }
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe(
+      response => {
+        // Autentificare reușită
+        this.router.navigate(['/people']); // Navighează către pagina principală
+      },
+      error => {
+        // Afișează un mesaj de eroare
+        console.error('Eroare de autentificare:', error);
+      }
+    );
+  }
 }
