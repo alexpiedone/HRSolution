461be9beab57c7900be16e2ad0bab2f17ce459ab
alexpiedone <alexdobre229@yahoo.ro>
2025-04-04 16:24:46 +0300

    1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import {MenubarModule} from 'primeng/menubar'
+import { MegaMenuItem, MenuItem } from 'primeng/api';
+import {CommonModule} from '@angular/common'
+import {MegaMenu} from 'primeng/megamenu'
+import {Avatar} from 'primeng/avatar'
+
 @Component({
   selector: 'app-home',
-  imports: [MenubarModule],
+  imports: [ MegaMenu, Avatar, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  items: MegaMenuItem[] = [];
 
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: '/home', root: true },
+      { label: 'Requests', icon: 'pi pi-fw pi-reply', routerLink: '/requests', root: true },
+      { label: 'Evaluations', icon: 'pi pi-fw pi-trophy', routerLink: '/evaluations', root: true },
+      { label: 'Documents', icon: 'pi pi-fw pi-pen-to-square', routerLink: '/documents', root: true },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog', routerLink: '/settings', root: true },
+    ];
+    
+  }
 }
