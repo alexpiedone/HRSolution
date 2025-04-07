0a3d248b7ffd52a04855fb1fc1d47c77d8f23797
alexpiedone <alexdobre229@yahoo.ro>
2025-04-07 22:41:38 +0300

    8

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,24 +3,32 @@ import { MegaMenuItem, MenuItem } from 'primeng/api';
 import {CommonModule} from '@angular/common'
 import {MegaMenu} from 'primeng/megamenu'
 import {Avatar} from 'primeng/avatar'
+import {RouterModule} from '@angular/router'
+import {MegaMenuModule} from 'primeng/megamenu'
 
+import { SidebarModule } from 'primeng/sidebar';
+import { PanelMenuModule } from 'primeng/panelmenu';
 @Component({
   selector: 'app-home',
-  imports: [ MegaMenu, Avatar, CommonModule],
+  imports: [ RouterModule, CommonModule, SidebarModule, PanelMenuModule, MegaMenuModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   items: MegaMenuItem[] = [];
-
+  collapsed = false;
+  visibleSidebar: boolean = false;
   ngOnInit() {
     this.items = [
-      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: '/home', root: true },
-      { label: 'Requests', icon: 'pi pi-fw pi-reply', routerLink: '/requests', root: true },
-      { label: 'Evaluations', icon: 'pi pi-fw pi-trophy', routerLink: '/evaluations', root: true },
-      { label: 'Documents', icon: 'pi pi-fw pi-pen-to-square', routerLink: '/documents', root: true },
-      { label: 'Settings', icon: 'pi pi-fw pi-cog', routerLink: '/settings', root: true },
+      { label: 'Home', icon: 'pi  pi-home', routerLink: '/home', root: true },
+      { label: 'Requests', icon: 'pi  pi-reply', routerLink: '/requests', root: true },
+      { label: 'Evaluations', icon: 'pi  pi-trophy', routerLink: '/evaluations', root: true },
+      { label: 'Documents', icon: 'pi  pi-pen-to-square', routerLink: '/documents', root: true },
+      { label: 'Settings', icon: 'pi  pi-cog', routerLink: '/settings', root: true },
     ];
-    
+  }
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
   }
 }
