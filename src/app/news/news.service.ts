adee91e392f21e468b867e85c98bd97568feff5b
alexpiedone <alexdobre229@yahoo.ro>
2025-04-17 18:32:56 +0300

    news item added

---
+++ src/app/news/news.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NewsItem } from '../models/newsitem';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  private baseUrl = 'https://localhost:7124/api/News';
+
+  constructor(private http: HttpClient) {}
+
+  getAllNews(): Observable<NewsItem[]> {
+    return this.http.get<NewsItem[]>(`${this.baseUrl}/GetAll`);
+  }
+}
